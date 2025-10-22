9214ec02d1c3c799e432ffa8e6f24901502a095e
tanushbhootra576 <193266446+tanushbhootra576@users.noreply.github.com>
2025-10-22 15:16:28 +0530

    layout

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Layout from '../components/layout/Layout';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -19,7 +20,9 @@ const DUMMY_MEETUPS = [
 
 function HomePage() {
     return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <Layout>
+            <MeetupList meetups={DUMMY_MEETUPS} />
+        </Layout>
     );
 }
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,12 +1,12 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-
+import Layout from "../../components/layout/Layout";
 function NewMeetupPage() {
 
     function addMeetupHandler(enteredMeetupData) {
         console.log(enteredMeetupData);
     }
 
-    return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    return <Layout><NewMeetupForm onAddMeetup={addMeetupHandler} /></Layout>;
 }
 
 
